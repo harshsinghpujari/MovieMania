b58530bb871e29617d879fbafbbcbb73c484619a
Himanshu Singh <himanshusingh83683@gmail.com>
2025-09-19 22:47:05 +0530

    Footer and Header component are done making responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,42 +3,47 @@ import { FaTwitter,FaInstagram, FaGithub } from 'react-icons/fa'
 
 function Footer() {
   return (
-    <footer className=" w-full h-38  bg-gray-900 text-gray-300 ">
-      <div className="flex justify-around gap-20 ">
+    <footer className=" w-full h-40 md:38  bg-gray-900 text-gray-300 ">
+      <div className="flex flex-col md:flex-row">
         
         {/* Brand */}
-        <div className='w-1/5 mt-6 mr-60 text-center items-center'>
-          <div className="text-3xl font-bold">
-            🎬 Movie Mania
-            <p className="text-lg text-gray-400">Your movie discovery app</p>
+        
+          <div className="w-full flex flex-col justify-center items-center mt-2 md:items-start md:ml-10">
+            <h2 className='text-xl font-bold '>🎬Movie Mania</h2>
+            <p className="text-md font-bold ml-2 text-gray-400">Your movie discovery app</p>
           </div>
-        </div>
+        
 
         <div className='flex gap-24 items-center '>
 
           {/* Links */}
-          <div className="flex  gap-10 ">
+          {/* <div className=" hidden md:flex  gap-10 ">
             <NavLink to="/" className="hover:text-white">Home</NavLink>
             <NavLink to="/About" className="hover:text-white">About</NavLink>
             <NavLink to="/Contact" className="hover:text-white">Contact</NavLink>
-          </div>
+          </div> */}
 
           {/* Socials */}
-          <div className="mt-12 gap-4">
-            <p>Socials:</p>
-              <div className='flex gap-8 text-white mt-2'>
-                <a href="#"><FaTwitter size={20}/> X</a>
-                <a href="#"><FaInstagram size={20}/> Insta</a>
-                <a href="#"><FaGithub size={20}/> Github</a>
+          <div className="w-full flex justify-center mt-4  md:gap-4 md:items-end mr-10 md:mt-10 mb-5 ">
+            <p className='hidden'>Socials:</p>
+              <div className='flex gap-10 text-white mt-2 ml-2'>
+                <a href="#"><FaTwitter size={20}/> <p className='hidden md:block'>X</p></a>
+                <a href="#"><FaInstagram size={20}/><p className='hidden md:block'>Insta</p></a>
+                <a href="#"><FaGithub size={20}/><p className='hidden md:block'>Github</p></a>
               </div>
           </div>
         </div>
       </div>
 
       {/* Copyright */}
-      <div className=" flex justify-center text-center text-sm text-gray-500 mt-2">
+      <div className=" hidden md:flex justify-center text-center text-sm text-gray-500 mt-2">
         © 2025 MovieHub. Author - Himanshu Singh.
       </div>
+
+      <div className=" flex justify-center text-center text-sm text-gray-500 mt-6 md:hidden">
+        © Author - Himanshu Singh.
+      </div>
+      
     </footer>
 
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,47 @@
 import { NavLink } from 'react-router'
+import { useState } from 'react'
+import { FaBars, FaTimes } from 'react-icons/fa'
+
 
 function Header() {
+
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  }
+
   return (
-    <header className='w-full h-16 fixed top-0 z-50 bg-slate-900'>
-      <nav className='text-2xl items-center flex justify-between px-10 py-2 mt-2'>
+    <header className=' w-full h-16 fixed top-0 z-50 bg-gray-900'>
+      <nav className='flex justify-between text-xl md:text-2xl items-center py-2 mt-2'>
         <div>
           <p>🎬 Movie Mania</p>
-          <img src="" alt="" />
         </div>
 
-        <div className='flex gap-8'>
+        <div className='mr-4 md:hidden'>
+          <button
+            onClick={toggleMenu}
+          >
+            {isMenuOpen ? <FaTimes size={24} /> : <FaBars size={24} />}
+          </button>
+        </div>
+
+        <div className=' hidden md:flex gap-8 mr-4'>
           <NavLink to="/">Home</NavLink>
           <NavLink to="/about">About</NavLink>
           <NavLink to="/contact">Contact</NavLink>
         </div>
         
       </nav>
+
+      {isMenuOpen && (
+        <div className='md:hidden absolute top-16 right-0 w-48 bg-slate-900 flex flex-col items-center gap-4 py-4 transition-transform duration-300 ease-in-out'>
+          <NavLink to="/" onClick={toggleMenu} className='hover:text-yellow-300'>Home</NavLink>
+          <NavLink to="/about" onClick={toggleMenu} className='hover:text-yellow-300'>About</NavLink>
+          <NavLink to="/contact" onClick={toggleMenu} className='hover:text-yellow-300'>Contact</NavLink>
+        </div>
+      )}
+
     </header>
   )
 }
